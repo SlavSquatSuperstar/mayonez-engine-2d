5fec8d695bb0b8491a435fc4055b82b94e4ae2bd
SlavSquatSuperstar <ericweng03@gmail.com>
2023-08-31 11:12:38 -0400

    Delete root run scripts
    Removals
    - Deleted run scripts from root folder (use scripts in dist folder instead)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ private var macOS = osName.startsWith("Mac")
 allprojects {
     // Name
     group = "slavsquatsuperstar"
-    version = "0.7.10-pre2"
+    version = "0.7.10-pre3"
 
     // Dependencies
     repositories { mavenCentral() }
