b4e8c33cea26eb6640562809319342483ab33fdf
SlavSquatSuperstar <ericweng03@gmail.com>
2023-12-28 13:14:40 +0800

    UI border box texture
    Additions
    - Added dark border texture
    - Weapon selection now uses a texture for the border box

--- mayonez-demos/src/main/java/slavsquatsuperstar/demos/spacegame/ui/WeaponSelectPanel.java
+++ mayonez-demos/src/main/java/slavsquatsuperstar/demos/spacegame/ui/WeaponSelectPanel.java
@@ -18,16 +18,17 @@ public class WeaponSelectPanel extends Script {
     // Constants
     private static final Texture BACKGROUND_TEXTURE = Textures.getTexture(
             "assets/spacegame/textures/ui_background.png");
-    private static final Color SELECTED_COLOR = Colors.GRAY;
-    private static final Color UNSELECTED_COLOR = Colors.WHITE;
-    private static final float SELECTED_MARGIN = 7.5f;
-    private static final float UNSELECTED_MARGIN = 5f;
+    private static final Texture BORDER_TEXTURE =  Textures.getTexture(
+            "assets/spacegame/textures/ui_border_dark.png");
+
+    private static final float BACKGROUND_MARGIN = 5f;
+    private static final float BORDER_MARGIN = 10f;
 
     // Fields
     private final Vec2 position, size;
     private final Color[] boxColors;
     private final UIBox[] backgrounds;
-    private int selectedIndex;
+    private UIBox border;
 
     public WeaponSelectPanel(Vec2 position, Vec2 size, Color... boxColors) {
         this.position = position;
@@ -46,17 +47,24 @@ public class WeaponSelectPanel extends Script {
 
         for (int i = 0; i < boxColors.length; i++) {
             var currPos = position.add(boxSpacing.mul(i));
-            backgrounds[i] = new UIBox(currPos, size.add(new Vec2(UNSELECTED_MARGIN)), BACKGROUND_TEXTURE);
+            backgrounds[i] = new UIBox(currPos, size.add(new Vec2(BACKGROUND_MARGIN)), BACKGROUND_TEXTURE);
             gameObject.addComponent(backgrounds[i]);
 
-            var box = new UIBox(currPos, size, ProjectileType.PROJECTILE_SPRITES.getTexture(i)) {
+            var icon = new UIBox(currPos, size, ProjectileType.PROJECTILE_SPRITES.getTexture(i)) {
                 @Override
                 public int getZIndex() {
-                    return super.getZIndex() + 1; // display above outline
+                    return super.getZIndex() + 1; // display above background
                 }
             };
-            gameObject.addComponent(box);
+            gameObject.addComponent(icon);
         }
+        border = new UIBox(position, size.add(new Vec2(BORDER_MARGIN)), BORDER_TEXTURE) {
+            @Override
+            public int getZIndex() {
+                return super.getZIndex() + 2; // display above icon
+            }
+        };
+        gameObject.addComponent(border);
 
         setSelection(0);
     }
@@ -68,15 +76,8 @@ public class WeaponSelectPanel extends Script {
      */
     public void setSelection(int index) {
         if (!IntMath.inRange(index, 0, backgrounds.length)) return;
-
-        // Unselect last
-        backgrounds[selectedIndex].setColor(UNSELECTED_COLOR);
-        backgrounds[selectedIndex].setSize(size.add(new Vec2(UNSELECTED_MARGIN)));
-
-        // Select current
-        selectedIndex = index;
-        backgrounds[selectedIndex].setColor(SELECTED_COLOR);
-        backgrounds[selectedIndex].setSize(size.add(new Vec2(SELECTED_MARGIN)));
+        // Move border
+        border.setPosition(backgrounds[index].getPosition());
     }
 
 }
