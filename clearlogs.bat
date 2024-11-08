56d0b8f9de6b6a19e92854d90c886a3e9bb39930
SlavSquatSuperstar <ericweng03@gmail.com>
2024-11-08 16:53:33 -0500

    Delete log files in all subdirectories
    Changes
    - clearlogs scrips now delete log files within subdirectories (including dist and mayonez-demos)
    
    Removals
    - Delete package scripts since Gradle package tasks are working

--- clearlogs.bat
+++ clearlogs.bat
@@ -9,7 +9,7 @@ rem Navigate to the project directory
 set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR% || exit /b 1
 
-rem Delete log folder outputs
-rmdir /s /q logs\ mayonez-demos\logs\
-del hs_err_pid*.log mayonez-demos\hs_err_pid*.log
+rem Delete log folder outputs and crash reports
+for /d /r %%i in (logs) do if exist "%%i" rd /s /q "%%i"
+for /r %%i in (hs_err_pid*.log) do if exist "%%i" del /q "%%i"
 echo Cleared all log files.
