6089f8d5ed99ff8795e66ba37799323bc07de5a7
SlavSquatSuperstar <ericweng03@gmail.com>
2025-02-25 11:51:28 -0500

    Create shield damaged effect

--- mayonez-demos/src/main/java/slavsquatsuperstar/demos/spacegame/combat/ShieldedDamageable.java
+++ mayonez-demos/src/main/java/slavsquatsuperstar/demos/spacegame/combat/ShieldedDamageable.java
@@ -1,5 +1,9 @@
 package slavsquatsuperstar.demos.spacegame.combat;
 
+import mayonez.*;
+import mayonez.graphics.sprites.*;
+import mayonez.graphics.textures.*;
+import mayonez.math.*;
 import mayonez.scripts.*;
 
 /**
@@ -11,23 +15,39 @@ public class ShieldedDamageable extends Damageable {
 
     // Constants
     private static final float SHIELD_WAIT_TIME = 1.2f;
+    private static final float SHIELD_FLASH_TIME = 0.1f;
+    private static final Texture SHIELD_EFFECT_TEXTURE = Textures.getTexture(
+            "assets/spacegame/textures/combat/shield_effect.png"
+    );
 
     // Fields
     private final Counter shieldPoints;
     private final Timer shieldWaitTimer; // Wait to recharge after getting hit
+    private final Timer shieldFlashTimer; // Flash shield when getting hit
     private final float shieldRegen; // Shield hp per second
+    private Sprite shieldEffect; // Shield effect sprite
 
     public ShieldedDamageable(float maxHealth, float maxShield, float shieldRegen) {
         super(maxHealth);
         shieldPoints = new Counter(0, maxShield, maxShield);
         shieldWaitTimer = new Timer(SHIELD_WAIT_TIME);
+        shieldFlashTimer = new Timer(SHIELD_FLASH_TIME);
         this.shieldRegen = shieldRegen;
+    }
+
+    @Override
+    protected void init() {
+        shieldEffect = Sprites.createSprite(SHIELD_EFFECT_TEXTURE);
+        shieldEffect.setSpriteTransform(Transform.scaleInstance(new Vec2(1.1f)));
+        gameObject.addComponent(shieldEffect);
+        shieldEffect.setEnabled(false);
     }
 
     @Override
     protected void start() {
         super.start();
         shieldWaitTimer.setValue(0f);
+        shieldFlashTimer.setValue(0f);
     }
 
     @Override
@@ -40,6 +60,13 @@ public class ShieldedDamageable extends Damageable {
             shieldPoints.clampValue();
         } else {
             shieldWaitTimer.countDown(dt);
+        }
+
+        // Wait to hide shield effect
+        if (shieldFlashTimer.isReady()) {
+            shieldEffect.setEnabled(false);
+        } else {
+            shieldFlashTimer.countDown(dt);
         }
     }
 
@@ -62,6 +89,10 @@ public class ShieldedDamageable extends Damageable {
                 shieldPoints.setValue(0f);
                 super.onObjectDamaged(damage - shieldHealthLeft);
             }
+
+            // Flash shield
+            shieldFlashTimer.reset();
+            shieldEffect.setEnabled(true);
         }
     }
 
