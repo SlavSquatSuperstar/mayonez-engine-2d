4cf0c890953a574ae196695c28cc8bb8d888d377
SlavSquatSuperstar <ericweng03@gmail.com>
2025-02-28 22:37:29 -0500

    Revise record documentation

--- mayonez-base/src/main/kotlin/mayonez/util/Record.kt
+++ mayonez-base/src/main/kotlin/mayonez/util/Record.kt
@@ -2,9 +2,13 @@ package mayonez.util
 
 /**
  * Stores information under key-value pairs, similar to how objects store
- * data in fields. Records store simple data types, including numbers,
- * texts, booleans, arrays, and other records, and may be converted to and
- * from a JSONObject.
+ * data in fields. This class is intended to store simple data types, including
+ * numbers, texts, booleans, arrays, and other records, similar to SQL records
+ * and JavaScript objects The [mayonez.assets.text.JSONFile] class can also
+ * convert Records to and from JSONObjects.
+ *
+ * Storing other data types in a Record may  make (de-)serialization difficult,
+ * in which case a dedicated Java class may be preferable.
  *
  * @author SlavSquatSuperstar
  */
@@ -18,14 +22,20 @@ open class Record(map: Map<String?, Any?>) {
     // Get Property Methods
 
     /**
-     * Retrieves the value stored under this key as a Java [Object], or null if
-     * it does not exist.
+     * Retrieves the value stored under this key as a Java [Object], or null
+     * if the key does not exist. The value itself may also be stored as null.
+     *
+     * @param key the key
+     * @return the object
      */
     operator fun get(key: String?): Any? = map[key]
 
     /**
-     * Retrieves the array stored under this key as a [List], or null if it
-     * does not exist.
+     * Retrieves the list or array stored under this key as a generic [List]
+     * of [Object]s, or null otherwise.
+     *
+     * @param key the key
+     * @return the list
      */
     fun getArray(key: String?): List<Any?>? {
         return when (val value = map[key]) {
@@ -36,8 +46,11 @@ open class Record(map: Map<String?, Any?>) {
     }
 
     /**
-     * Retrieves the object stored under this key as a [Record], or null if it
-     * does not exist.
+     * Retrieves the record or map stored under this key as another [Record],
+     * or null otherwise.
+     *
+     * @param key the key
+     * @return the record
      */
     fun getObject(key: String?): Record? {
         return when (val value = map[key]) {
@@ -49,14 +62,20 @@ open class Record(map: Map<String?, Any?>) {
 
     /**
      * Retrieves the value stored under this key as a string, or an empty
-     * string if it does not exist.
+     * string otherwise.
+     *
+     * @param key the key
+     * @return the string
      */
     fun getString(key: String?): String = map[key]?.toString() ?: ""
 
     /**
-     * Retrieves the value stored under this key as a boolean, or false if it
-     * does not exist. If the value is stored as "true", "yes", or a non-zero
-     * number, then it will also be considered true.
+     * Retrieves the value stored under this key as a boolean. If the value is
+     * stored as "true", "yes", or a non-zero number, then it will also be
+     * considered true. Otherwise, the value will be null.
+     *
+     * @param key the key
+     * @return the boolean
      */
     fun getBoolean(key: String?): Boolean {
         return when (val value = map[key]) {
@@ -72,9 +91,13 @@ open class Record(map: Map<String?, Any?>) {
     }
 
     /**
-     * Retrieves the value stored under this key as an integer, or 0 if it does
-     * not exist. If the value is stored as a string or other number, then it
-     * will be converted and truncated into an integer.
+     * Retrieves the value stored under this key as an integer. If the value
+     * is a numerical string or other number, then it will be parsed and
+     * truncated into an integer. If the value is a boolean, then it will be
+     * considered 1 if true and 0 if false. Otherwise, the value will be 0.
+     *
+     * @param key the key
+     * @return the integer
      */
     fun getInt(key: String?): Int {
         return when (val value = map[key]) {
@@ -86,9 +109,13 @@ open class Record(map: Map<String?, Any?>) {
     }
 
     /**
-     * Retrieves the value stored under this key as a float, or 0 if it does
-     * not exist. If the number is stored as a string or other number, then
-     * it will be converted into a float.
+     * Retrieves the value stored under this key as a float. If the value is
+     * a numerical string or other number, then it will be parsed into a float.
+     * If the value is a boolean, then it will be considered 1 if true and 0 if
+     * false. Otherwise, the value will be 0.
+     *
+     * @param key the key
+     * @return the float
      */
     fun getFloat(key: String?): Float {
         return when (val value = map[key]) {
@@ -101,42 +128,85 @@ open class Record(map: Map<String?, Any?>) {
 
     // Set Property Methods
 
-    /** Stores or updates an integer under this key. */
+    /**
+     * Stores or updates an integer under this key.
+     *
+     * @param key the key
+     * @param value the integer
+     */
     operator fun set(key: String?, value: Int?) {
         map[key] = value
     }
 
-    /** Stores or updates a float under this key. */
+    /**
+     * Stores or updates a float under this key.
+     *
+     * @param key the key
+     * @param value the float
+     */
     operator fun set(key: String?, value: Float?) {
         map[key] = value
     }
 
-    /** Stores or updates a boolean under this key. */
+    /**
+     * Stores or updates a boolean under this key.
+     *
+     * @param key the key
+     * @param value the boolean
+     */
     operator fun set(key: String?, value: Boolean?) {
         map[key] = value
     }
 
-    /** Stores or updates a string under this key. */
+    /**
+     * Stores or updates a string under this key.
+     *
+     * @param key the key
+     * @param value the string
+     */
     operator fun set(key: String?, value: String?) {
         map[key] = value
     }
 
-    /** Stores or updates a list under this key. */
+    /**
+     * Stores or updates a list under this key. The list will be converted to
+     * a generic [Object] list.
+     *
+     * @param key the key
+     * @param value the list
+     */
     operator fun set(key: String?, value: List<*>?) {
         map[key] = value as List<Any?>
     }
 
-    /** Stores or updates an array under this key. */
+    /**
+     * Stores or updates am array under this key. The array will be converted
+     * to a generic [Object] list.
+     *
+     * @param key the key
+     * @param value the array
+     */
     operator fun set(key: String?, value: Array<*>?) {
         map[key] = value?.asList<Any?>()
     }
 
-    /** Stores or updates a record under this key. */
+    /**
+     * Stores or updates another record under this key.
+     *
+     * @param key the key
+     * @param value the record
+     */
     operator fun set(key: String?, value: Record?) {
         map[key] = value
     }
 
-    /** Stores or updates a map under this key. */
+    /**
+     * Stores or updates a map under this key. The map will be converted into
+     * another record
+     *
+     * @param key the key
+     * @param value the map
+     */
     operator fun set(key: String?, value: Map<*, *>?) {
         map[key] = value?.toRecord()
     }
@@ -144,7 +214,7 @@ open class Record(map: Map<String?, Any?>) {
     // Copy Methods
 
     /**
-     * Create a copy of this record with all the same stored values.
+     * Create a copy of this record with all the same stored keys-value pairs.
      *
      * @return the copy
      */
@@ -183,7 +253,7 @@ open class Record(map: Map<String?, Any?>) {
     fun size(): Int = map.size
 
     /**
-     * Converts this record to an object-to-object map.
+     * Converts this record to a generic [Object]-to-[Object] map.
      *
      * @return a map
      */
@@ -191,7 +261,13 @@ open class Record(map: Map<String?, Any?>) {
 
     // Object Overrides
 
-    /** Whether any value is stored in the record under the given key. */
+    /**
+     * Checks whether any value is stored in the record under the given key.
+     * At most one value may be stored under a null key.
+     *
+     * @param key the key
+     * @return if the key exists
+     */
     operator fun contains(key: String?): Boolean = map.containsKey(key)
 
     override fun equals(other: Any?): Boolean {
