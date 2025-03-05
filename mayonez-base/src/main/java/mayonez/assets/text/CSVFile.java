9a73ea8eb88e0268609d16fe36acabda34241fd6
SlavSquatSuperstar <ericweng03@gmail.com>
2025-03-04 20:19:44 -0500

    Write complex CSV files with Apache CSV

--- mayonez-base/src/main/java/mayonez/assets/text/CSVFile.java
+++ mayonez-base/src/main/java/mayonez/assets/text/CSVFile.java
@@ -5,9 +5,7 @@ import mayonez.assets.*;
 import mayonez.util.Record;
 import org.apache.commons.csv.CSVFormat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -17,11 +15,13 @@ import java.util.*;
  * <p>
  * Usage: Newlines are used as record separators, commas are used as field delimiters,
  * and quotes are used to escape newline, comma, and quote characters.
- * Additionally, the first row contains headers for all the record fields
+ * Additionally, the first row contains headers for all the record fields.
  *
  * @author SlavSquatSuperstar
  */
 public class CSVFile extends Asset {
+
+    private static final CSVFormat CSV_FORMAT = CSVFormat.DEFAULT;
 
     private String[] headers;
 
@@ -30,20 +30,23 @@ public class CSVFile extends Asset {
     }
 
     /**
-     * Parses the CSV data in this file and returns a list of {@link mayonez.util.Record} objects.
+     * Parses the CSV data in this file and returns a list of {@link mayonez.util.Record}
+     * objects. Note that the program reads all values strings and does not attempt
+     * to parse them into other types
      *
      * @return the records, empty if the file does not exist
      */
     public List<Record> readCSV() {
         var records = new ArrayList<Record>();
-        try (var reader = new BufferedReader(new InputStreamReader(openInputStream()))) {
-            var parser = CSVFormat.DEFAULT;
-            var lines = parser.parse(reader).getRecords();
+        try (var reader = TextIOUtils.getReader(openInputStream())) {
+            var lines = CSV_FORMAT.parse(reader).getRecords();
             if (lines.isEmpty()) return records; // No lines
 
+            // Get headers
             this.headers = lines.getFirst()
-                    .toList().toArray(new String[0]); // Get headers
+                    .toList().toArray(new String[0]);
 
+            // Get records
             for (var row = 1; row < lines.size(); row++) {
                 var line = lines.get(row);
                 var rec = getRecordFromLine(line.values());
@@ -72,27 +75,29 @@ public class CSVFile extends Asset {
      * @param headers the table headers
      */
     public void saveCSV(List<Record> records, String[] headers) {
-        var csvLines = new String[records.size() + 1];
-        csvLines[0] = String.join(",", headers); // add headers
+        try (
+                var writer = TextIOUtils.getWriter(openOutputStream(false));
+                var printer = CSV_FORMAT.print(writer)
+        ) {
+            // Write headers
+            printer.printRecord((Object[]) headers);
 
-        for (var row = 0; row < records.size(); row++) {
-            csvLines[row + 1] = getLineFromRecord(records, headers, row);
-        }
-        try (var stream = openOutputStream(false)) {
-            TextIOUtils.write(stream, csvLines);
+            // Write records
+            for (var record : records) {
+                var fields = getLineFromRecord(record, headers);
+                printer.printRecord((Object[]) fields);
+            }
         } catch (IOException e) {
             Logger.error("Could not save to file %s", getFilename());
         }
     }
 
-    private String getLineFromRecord(List<Record> records, String[] headers, int row) {
-        var rec = records.get(row);
-        var csvVals = new String[headers.length];
+    private String[] getLineFromRecord(Record record, String[] headers) {
+        var fields = new String[headers.length];
         for (var col = 0; col < headers.length; col++) {
-            csvVals[col] = rec.getString(headers[col]);
-            // TODO commas
+            fields[col] = record.getString(headers[col]);
         }
-        return String.join(",", csvVals);
+        return fields;
     }
 
     public String[] getHeaders() {
--- mayonez-base/src/main/java/mayonez/assets/text/TextIOUtils.java
+++ mayonez-base/src/main/java/mayonez/assets/text/TextIOUtils.java
@@ -13,24 +13,31 @@ public final class TextIOUtils {
     private TextIOUtils() {
     }
 
-    private static List<String> read(InputStream input) throws IOException {
-        if (input == null) {
-            throw new IOException("File does not exist or is not readable");
-        }
-        try {
-            var reader = new BufferedReader(new InputStreamReader(input));
-            List<String> lines = new ArrayList<>();
+    // Reader/Writer Methods
 
-            var line = reader.readLine();
-            while (line != null) {
-                lines.add(line);
-                line = reader.readLine();
-            }
-            return lines;
-        } catch (IOException e) {
-            throw new IOException("Error while reading file");
-        }
+    /**
+     * Create a {@link BufferedReader} that will read text from the given
+     * input stream.
+     *
+     * @param input the input stream
+     * @return the buffered reader
+     */
+    public static BufferedReader getReader(InputStream input) {
+        return new BufferedReader(new InputStreamReader(input));
     }
+
+    /**
+     * Create a {@link BufferedWriter} that will write or append text to the
+     * given input stream.
+     *
+     * @param output the output stream
+     * @return the buffered writer
+     */
+    public static BufferedWriter getWriter(OutputStream output) {
+        return new BufferedWriter(new OutputStreamWriter(output));
+    }
+
+    // Read/Write Text Methods
 
     /**
      * Reads text from a stream as a single string.
@@ -60,12 +67,32 @@ public final class TextIOUtils {
         return read(input).toArray(new String[0]);
     }
 
+    private static List<String> read(InputStream input) throws IOException {
+        if (input == null) {
+            throw new IOException("File does not exist or is not readable");
+        }
+        try {
+            var reader = getReader(input);
+            List<String> lines = new ArrayList<>();
+
+            var line = reader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+            // Keep stream open
+            return lines;
+        } catch (IOException e) {
+            throw new IOException("Error while reading file");
+        }
+    }
+
     /**
-     * Writes any number of lines of text to a stream. A new line character
-     * ('\n') is inserted after every line.
+     * Writes or appends any number of lines of text to a stream. A new line
+     * character is inserted after every line.
      *
      * @param output the output stream
-     * @param lines the lines of text
+     * @param lines  the lines of text
      * @throws java.io.FileNotFoundException if the file cannot be written to
      */
     public static void write(OutputStream output, String... lines) throws IOException {
@@ -75,7 +102,7 @@ public final class TextIOUtils {
             return;
         }
         try {
-            var writer = new BufferedWriter(new OutputStreamWriter(output));
+            var writer = getWriter(output);
             for (var line : lines) {
                 if (line == null) continue;
                 writer.write(line);
