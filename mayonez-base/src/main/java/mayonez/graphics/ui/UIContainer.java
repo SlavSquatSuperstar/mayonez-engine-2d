951b3f394306f7245cc94cc4316c4e4027f14c9e
SlavSquatSuperstar <ericweng03@gmail.com>
2026-01-04 17:26:08 +0800

    Create UIContainer abstract class

---
+++ mayonez-base/src/main/java/mayonez/graphics/ui/UIContainer.java
@@ -0,0 +1,111 @@
+package mayonez.graphics.ui;
+
+import mayonez.*;
+
+import java.util.*;
+
+/**
+ * A user interface element that stores and manages the position and size of other elements
+ * according to a layout. Containers may store renderable components or other containers.
+ *
+ * @author SlavSquatSuperstar
+ */
+public abstract class UIContainer extends Script implements UIElement {
+
+    protected final List<UIElement> elements;
+    private boolean started; // Don't arrange until all elements added
+
+    public UIContainer() {
+        super(UpdateOrder.RENDER);
+        elements = new ArrayList<>();
+        started = false;
+    }
+
+    // Component Methods
+
+    @Override
+    protected void start() {
+        started = true;
+        arrangeElements();
+    }
+
+    @Override
+    protected void onDestroy() {
+        clearElements();
+        started = false;
+    }
+
+    // TODO onEnable/onDisable
+
+    // Layout Methods
+
+    /**
+     * Rearrange all child element positions and sizes. This method will also be recursively called
+     * for any child containers.
+     */
+    protected abstract void arrangeElements();
+
+
+    // Element Methods
+
+    /**
+     * Gets the child elements in the order they were added.
+     *
+     * @return the elements
+     */
+    public List<UIElement> getElements() {
+        return elements;
+    }
+
+    /**
+     * Gets the child element at the specified index.
+     *
+     * @param index the element index
+     * @return the element
+     */
+    public UIElement getElement(int index) {
+        return elements.get(index);
+    }
+
+    /**
+     * Gets the number of child elements in this container.
+     *
+     * @return the count
+     */
+    public int numElements() {
+        return elements.size();
+    }
+
+    /**
+     * Adds a UI element to this container and refreshes the layout. If this container has been
+     * added to a {@link mayonez.GameObject}, then the element will also be added to that object
+     * .
+     *
+     * @param element the element
+     */
+    public void addElement(UIElement element) {
+        if (gameObject != null) {
+            gameObject.addComponent((Component) element);
+        }
+        elements.add(element);
+        if (started) arrangeElements();
+    }
+
+    /**
+     * Removes a UI element from this container and refreshes the layout.
+     *
+     * @param element the element
+     */
+    public void removeElement(UIElement element) {
+        elements.remove(element);
+        if (started) arrangeElements();
+    }
+
+    /**
+     * Removes all UI elements from this container.
+     */
+    public void clearElements() {
+        elements.clear();
+    }
+
+}
